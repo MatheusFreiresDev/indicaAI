d4bd1b05d11e4da9f78a8dfc291f6a5c2a283b89
Matheus Freires <matheusfreires717@gmail.com>
2026-03-04 22:04:52 -0300

    first commit

---
+++ src/main/java/com/indicaAI/IndicaAiApplication.java
@@ -0,0 +1,14 @@
+package com.indicaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicaAiApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/indicaAI/IndicaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indicaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
